d61058185df71ca85c431545d59e64557421667e
hevilavio <bld_hsoares@uolinc.com>
2015-10-06 17:06:35 -0300

    Commit inicial

---
+++ src/main/java/sample/web/ui/App.java
@@ -0,0 +1,16 @@
+package sample.web.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hevilavio on 8/3/15.
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(App.class, args);
+    }
+}
